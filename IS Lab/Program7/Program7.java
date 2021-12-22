459bda406bcce90e65795dea02a62f9436240644
18wh1a1234 <69299896+18wh1a1234@users.noreply.github.com>
2021-12-22 12:40:47 +0530

    Update Program7.java

--- IS Lab/Program7/Program7.java
+++ IS Lab/Program7/Program7.java
@@ -5,16 +5,16 @@ import javax.swing.JOptionPane;
 
 public class BlowFishCipher {
     public static void main(String[] args) throws Exception {
-        KeyGeneratorkeygenerator = KeyGenerator.getInstance("Blowfish");
-        cipher = Cipher.getInstance("Blowfish");
+        KeyGenerator keygenerator = KeyGenerator.getInstance("Blowfish");
+        SecretKey secretkey = keygenerator.generateKey();
+        Cipher cipher = Cipher.getInstance("Blowfish");
         cipher.init(Cipher.ENCRYPT_MODE, secretkey);
         String inputText = JOptionPane.showInputDialog("Input your message: "); 
         byte[] encrypted = cipher.doFinal(inputText.getBytes());
         cipher.init(Cipher.DECRYPT_MODE, secretkey);
         byte[] decrypted = cipher.doFinal(encrypted);
-        JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),
-        "\nEncrypted text: " + new String(encrypted) + "\n" +
-        "\nDecrypted text: " + new String(decrypted));
-        System.exit(0);
+        System.out.println ("Original String : " +inputText);
+        System.out.println ("\nEncrypted text: " + new String(encrypted));
+        System.out.println ("\nDecrypted text: " + new String(decrypted));
     } 
 }
