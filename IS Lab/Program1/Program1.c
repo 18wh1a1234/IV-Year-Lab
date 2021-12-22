ea116a99cc2fae6c66af5b5bbf958e01e698d92c
18wh1a1234 <69299896+18wh1a1234@users.noreply.github.com>
2021-12-22 10:59:43 +0530

    Add files via upload

---
+++ IS Lab/Program1/Program1.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+
+int main() {
+	char str[] = "Hello World";
+	char str1[11];
+	int i, len;
+	len = strlen(str);
+	
+	for(i = 0; i < len; i++) {
+		str1[i] = str[i] ^ 0;
+		printf("%c", str1[i]);
+	}
+	printf("\n");
+	
+	return 0;
+}
+
